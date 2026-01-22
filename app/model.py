290ec782f9127249a8ad3d73baa19385570f9f5b
karima <karimabenihda@gmail.com>
2026-01-22 11:34:50 +0100

    Add Btc model and input schema for Bitcoin data representation

--- app/model.py
+++ app/model.py
@@ -1,4 +1,4 @@
-from sqlalchemy import Column, Integer, String
+from sqlalchemy import Column, Integer, String,DateTime,Float
 from sqlalchemy.ext.declarative import declarative_base
 
 Base = declarative_base()
@@ -9,3 +9,21 @@ class User(Base):
     id = Column(Integer, primary_key=True, index=True)
     email = Column(String, unique=True, index=True, nullable=False)
     password = Column(String, nullable=False)
+
+class Btc(Base):
+    open_time=Column(DateTime),
+    open=Column(Float),
+    high=Column(Float),
+    low=Column(Float),
+    close=Column(Float),
+    volume=Column(Float),
+    close_time=Column(Float),
+    quote_asset_volume=Column(DateTime),
+    number_of_trades=Column(Float),
+    taker_buy_base_volume=Column(Float),
+    taker_buy_quote_volume=Column(Float),
+    close_t_plus_10=Column(Float),
+    ma_05=Column(Float),
+    ma_10=Column(Float),
+    taker_ratio=Column(Float),
+       
